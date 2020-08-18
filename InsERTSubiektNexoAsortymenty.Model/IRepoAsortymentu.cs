f9fd2bd422035e7fe276655c9abfdbfbe148fdfa
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2020-08-18 17:49:01 +0200

    dodanie projektu odpowiedzialnego za pracę z API i pracę z danymi

---
+++ InsERTSubiektNexoAsortymenty.Model/IRepoAsortymentu.cs
@@ -0,0 +1,14 @@
+﻿using InsERT.Moria.Asortymenty;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InsERTSubiektNexoAsortymenty.Model
+{
+    public interface IRepoAsortymentu
+    {
+        ICollection<IAsortyment> GetAsortyment();
+
+        bool ZmienOpis(string Id, string nowyOpis);
+    }
+}
---
+++ InsERTSubiektNexoAsortymenty.Model/RepoAsortymentu.cs
@@ -0,0 +1,20 @@
+﻿using InsERT.Moria.Asortymenty;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InsERTSubiektNexoAsortymenty.Model
+{
+    public class RepoAsortymentu : IRepoAsortymentu
+    {
+        public ICollection<IAsortyment> GetAsortyment()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ZmienOpis(string Id, string nowyOpis)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
