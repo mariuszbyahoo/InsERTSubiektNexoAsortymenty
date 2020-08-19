9c051bc5083e11f60eca451ef2b7d8d5f20263e7
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2020-08-19 16:19:00 +0200

    dodanie testu przypadku pozytywnego metody DtoAsortymentu.ZmienOpis()

--- InsERTSubiektNexoAsortymenty.Data.Testy/TestyDTOAsortymentu.cs
+++ InsERTSubiektNexoAsortymenty.Data.Testy/TestyDTOAsortymentu.cs
@@ -10,18 +10,25 @@ namespace InsERTSubiektNexoAsortymenty.Data.Testy
     [TestClass]
     public class TestyDTOAsortymentu
     {
-        private readonly string symbolDostawy = "DOSTAWA";
-        private readonly int indexBalsamuNawilzajacego200 = 0;
-        private readonly int idBalsamuNawilzajacego200 = 100000;
-        private readonly int idDostawy = 100003;
+        private static DtoAsortymentu DTO;
+        private string zastanyOpis;
+        private readonly string _symbolDostawy = "DOSTAWA";
+        private readonly string _testowyOpis = "testowyOpis";
+        private readonly int _indexBalsamuNawilzajacego200 = 0;
+        private readonly int _idBalsamuNawilzajacego200 = 100000;
+        private readonly int _idDostawy = 100003;
+
+        [ClassInitialize]
+        public static void TestFixtureSetUp(TestContext ctx)
+        {
+            DTO = new DtoAsortymentu();
+        }
 
         [TestMethod]
         public void PodajWszystkieAsortymenty___Gdy_Zostanie_Wywolane_Poprawnie___Zwroci_Zawartosc_Tabeli_Asortymenty_Jako_Niepusta_Liste_Implementujaca_IBindingList()
         {
-            // Przygotuj
-            var dto = new DtoAsortymentu();
             // Działaj
-            var produkty = dto.PodajWszystkieAsortymenty();
+            var produkty = DTO.PodajWszystkieAsortymenty();
 
             // Sprawdź
             produkty.Count.Should().BeGreaterThan(0);
@@ -31,10 +38,8 @@ namespace InsERTSubiektNexoAsortymenty.Data.Testy
         [TestMethod]
         public void PodajWszystkieAsortymenty___Gdy_Zostanie_Wywolane_Poprawnie___Encje_Zawarte_W_Liscie_Beda_Typu_Asortyment()
         {
-            // Przygotuj
-            var dto = new DtoAsortymentu();
             // Działaj
-            var produkty = dto.PodajWszystkieAsortymenty();
+            var produkty = DTO.PodajWszystkieAsortymenty();
 
             // Sprawdź
             produkty[0].Should().BeOfType<Asortyment>();
@@ -43,25 +48,37 @@ namespace InsERTSubiektNexoAsortymenty.Data.Testy
         [TestMethod]
         public void PodajKonkretnyAsortyment___Gdy_Przekaze_Poprawny_Symbol_Dostawy___Zwroci_Konkretna_encje()
         {
-            // Przygotuj
-            var dto = new DtoAsortymentu();
             //Działaj
-            var asortyment = dto.PodajKonkretnyAsortyment(symbolDostawy);
+            var asortyment = DTO.PodajKonkretnyAsortyment(_symbolDostawy);
 
             // Sprawdź
-            asortyment.Dane.Id.Should().Be(idDostawy);
+            asortyment.Dane.Id.Should().Be(_idDostawy);
         }
 
         [TestMethod]
         public void PodajKonkretnyAsortyment___Gdy_Przekaze_Poprawny_Index_Balsamu_Nawilzajacego_200___Zwroci_Odpowiednia_Encje()
         {
-            // Przygotuj
-            var dto = new DtoAsortymentu();
             //Działaj
-            var asortyment = dto.PodajKonkretnyAsortyment(indexBalsamuNawilzajacego200);
+            var asortyment = DTO.PodajKonkretnyAsortyment(_indexBalsamuNawilzajacego200);
 
             // Sprawdź
-            asortyment.Id.Should().Be(idBalsamuNawilzajacego200);
+            asortyment.Id.Should().Be(_idBalsamuNawilzajacego200);
+        }
+
+        [TestMethod]
+        public void ZmienOpis___Gdy_Przekaze_Opis_O_Dopuszczalnej_Wielkosci___Zmieni_Opis_W_Danym_Wierszu_W_DB()
+        {
+            // Przygotuj
+            zastanyOpis = DTO.PodajKonkretnyAsortyment(_symbolDostawy).Dane.Opis;
+
+            // Działaj
+            DTO.ZmienOpis(_symbolDostawy, _testowyOpis);
+
+            // Sprawdź
+            DTO.PodajKonkretnyAsortyment(_symbolDostawy).Dane.Opis.Should().Be(_testowyOpis);
+
+            // Przywróć
+            DTO.ZmienOpis(_symbolDostawy, zastanyOpis);
         }
     }
 }
