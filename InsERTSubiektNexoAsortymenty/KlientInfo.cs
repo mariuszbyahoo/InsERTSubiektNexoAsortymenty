cad3ddc834ec8c78f274dfebbdd446e6ea8ce76a
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2020-08-20 18:39:13 +0200

    dodanie instrukcji warunkowej do metody ToString() w klasie KlientInfo

--- InsERTSubiektNexoAsortymenty/KlientInfo.cs
+++ InsERTSubiektNexoAsortymenty/KlientInfo.cs
@@ -4,17 +4,21 @@ namespace InsERTSubiektNexoAsortymenty
 {
     public class KlientInfo
     {
-        public string NazwaSkrocona { get; set; }
+        public string Nazwa { get; set; }
         public string NIP { get; set; }
+        public int Id { get; set; }
         public KlientInfo(Podmiot podmiot)
         {
-            NazwaSkrocona = podmiot.NazwaSkrocona;
+            Nazwa = podmiot.NazwaSkrocona;
             NIP = podmiot.NIP;
+            Id = podmiot.Id;
         }
 
         public override string ToString()
         {
-            return $"{this.NazwaSkrocona}, NIP: {this.NIP}";
+            var rezultat = this.Nazwa;
+            if (NIP.Length > 0) rezultat += $", NIP: {this.NIP}";
+            return rezultat;
         }
     }
 }
