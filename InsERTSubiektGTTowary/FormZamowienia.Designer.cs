8098d93563f66b064ba10b755793839b78db2921
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2020-08-26 13:59:51 +0200

    dodanie przycisków bez ich obsługi do formularza zamówienia

--- InsERTSubiektGTTowary/FormZamowienia.Designer.cs
+++ InsERTSubiektGTTowary/FormZamowienia.Designer.cs
@@ -32,6 +32,9 @@
             this.comboBoxEdit1 = new DevExpress.XtraEditors.ComboBoxEdit();
             this.gridControl1 = new DevExpress.XtraGrid.GridControl();
             this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.splitterControl1 = new DevExpress.XtraEditors.SplitterControl();
+            this.przyciskAnuluj = new DevExpress.XtraEditors.SimpleButton();
+            this.przyciskOk = new DevExpress.XtraEditors.SimpleButton();
             ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
             this.panelControl1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.comboBoxEdit1.Properties)).BeginInit();
@@ -41,16 +44,19 @@
             // 
             // panelControl1
             // 
+            this.panelControl1.Controls.Add(this.przyciskOk);
+            this.panelControl1.Controls.Add(this.przyciskAnuluj);
+            this.panelControl1.Controls.Add(this.splitterControl1);
             this.panelControl1.Controls.Add(this.comboBoxEdit1);
             this.panelControl1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panelControl1.Location = new System.Drawing.Point(0, 321);
+            this.panelControl1.Location = new System.Drawing.Point(0, 268);
             this.panelControl1.Name = "panelControl1";
-            this.panelControl1.Size = new System.Drawing.Size(816, 47);
+            this.panelControl1.Size = new System.Drawing.Size(816, 100);
             this.panelControl1.TabIndex = 0;
             // 
             // comboBoxEdit1
             // 
-            this.comboBoxEdit1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboBoxEdit1.Dock = System.Windows.Forms.DockStyle.Top;
             this.comboBoxEdit1.EditValue = "Wybierz Kontrahenta";
             this.comboBoxEdit1.Location = new System.Drawing.Point(2, 2);
             this.comboBoxEdit1.Name = "comboBoxEdit1";
@@ -68,7 +74,7 @@
             this.gridControl1.Location = new System.Drawing.Point(0, 0);
             this.gridControl1.MainView = this.gridView1;
             this.gridControl1.Name = "gridControl1";
-            this.gridControl1.Size = new System.Drawing.Size(816, 321);
+            this.gridControl1.Size = new System.Drawing.Size(816, 268);
             this.gridControl1.TabIndex = 1;
             this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
             this.gridView1});
@@ -77,6 +83,33 @@
             // 
             this.gridView1.GridControl = this.gridControl1;
             this.gridView1.Name = "gridView1";
+            // 
+            // splitterControl1
+            // 
+            this.splitterControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.splitterControl1.Location = new System.Drawing.Point(2, 42);
+            this.splitterControl1.Name = "splitterControl1";
+            this.splitterControl1.Size = new System.Drawing.Size(812, 12);
+            this.splitterControl1.TabIndex = 1;
+            this.splitterControl1.TabStop = false;
+            // 
+            // przyciskAnuluj
+            // 
+            this.przyciskAnuluj.Dock = System.Windows.Forms.DockStyle.Left;
+            this.przyciskAnuluj.Location = new System.Drawing.Point(2, 54);
+            this.przyciskAnuluj.Name = "przyciskAnuluj";
+            this.przyciskAnuluj.Size = new System.Drawing.Size(94, 44);
+            this.przyciskAnuluj.TabIndex = 2;
+            this.przyciskAnuluj.Text = "Anuluj";
+            // 
+            // przyciskOk
+            // 
+            this.przyciskOk.Dock = System.Windows.Forms.DockStyle.Right;
+            this.przyciskOk.Location = new System.Drawing.Point(727, 54);
+            this.przyciskOk.Name = "przyciskOk";
+            this.przyciskOk.Size = new System.Drawing.Size(87, 44);
+            this.przyciskOk.TabIndex = 3;
+            this.przyciskOk.Text = "OK";
             // 
             // FormZamowienia
             // 
@@ -101,5 +134,8 @@
         private DevExpress.XtraEditors.ComboBoxEdit comboBoxEdit1;
         private DevExpress.XtraGrid.GridControl gridControl1;
         private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraEditors.SimpleButton przyciskOk;
+        private DevExpress.XtraEditors.SimpleButton przyciskAnuluj;
+        private DevExpress.XtraEditors.SplitterControl splitterControl1;
     }
 }
